c40471171b7df95cc3f53693150166fc9fe383c5
melon <meloniusz@hotmail.com>
2019-10-25 13:24:24 +0800

    changes to js - mainly functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,147 @@
+//--Setting global variables for the Quiz--//
+let queryURL = "https://opentdb.com/api.php?amount=10&category=9&type=multiple";
+
+let apiQuestions = [];
+let correctAnswer = [];
+let optionB = [];
+let optionC = [];
+let optionD = [];
+let counter = 0;
+
+let wins = 0;
+let losses = 0;
+let skips = 0;
+let timer = 0;
+let timeLeft = 15;
+let intervalId;
+
 $(document).ready(function() {
-  console.log("ready!");
-  $("#questionBox").hide();
-});
-
-//Query for API random questions//
-$(window).on("load", function() {
-  var queryURL =
-    "https://opentdb.com/api.php?amount=10&category=9&type=multiple";
-
+  //--This function will send a query to API Database for random quiz question--//
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    console.log(response); //--Ensures our object contains all API--//
+
+    for (let i = 0; i < response.results.length; i++) {
+      apiQuestions.push(response.results[i].question);
+      correctAnswer.push(response.results[i].correct_answer);
+      optionB.push(response.results[i].incorrect_answers[0]);
+      optionC.push(response.results[i].incorrect_answers[1]);
+      optionD.push(response.results[i].incorrect_answers[2]);
+    }
   });
 });
 
-//create questions//
-let questions = [];
-
-//variables in quiz//
-$("#quizStart").on("click", function() {
-  let startButton = $("#quizStart");
-  $(startButton).fadeToggle("slow", startQuestion);
+$("#questionBox").hide();
+$("#reset").hide();
+let muzyka = new Audio("./assets/music/dynamic.mp3");
+let playMusic = $("#quizStart").on("click", () => {
+  muzyka.play();
+  intervalId = setInterval(timeIt, 1000);
 });
 
-let startQuestion = () => {
+//--Setting up our timer for each question here:--//
+
+let qtimer = $("#timer");
+qtimer.html(convertSeconds(timeLeft - timer));
+
+function timeIt() {
+  timer++;
+  qtimer.html(convertSeconds(timeLeft - timer));
+  if (timeLeft - timer === 0) {
+    unanswered();
+  }
+}
+
+function convertSeconds(s) {
+  let min = Math.floor(s / 60);
+  let sec = s % 60;
+  return min + ":" + sec;
+}
+//--Timer function finishes here--//
+function loadPage() {
   $("#quizStart").hide();
   $("#imgBox").hide();
   $("#questionBox").show();
-  let timer = $("<h2>");
-  let question = $("<h2>");
+  loadNextQuestion();
+}
 
-  startQuestion();
-};
+function gotItRight() {
+  console.log("You got it right!");
+  wins++;
+  timer = -1;
+  loadNextQuestion();
+}
+
+function unanswered() {
+  console.log("You didn't try to answer");
+  skips++;
+  timer = -1;
+  loadNextQuestion();
+}
+
+function gotItWrong() {
+  console.log("You got it wrong!"); //make it display <h3> instead in #answer-status div!
+  //display the correct message in #correct-answer div!
+  losses++;
+  timer = -1;
+  loadNextQuestion();
+}
+//--This function will shuffle our buttons so optionA(correct ans.) will always change its place//
+function shuffle(allMyButtons) {
+  for (i = allMyButtons.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = allMyButtons[i];
+    allMyButtons[i] = allMyButtons[j];
+    allMyButtons[j] = x;
+  }
+}
+function gameFinished() {
+  if (wins + losses + skips === counter) {
+    alert("gówno");
+    $("#timer").hide();
+    clearInterval(intervalId);
+  }
+}
+
+function loadNextQuestion() {
+  $("#question").html(apiQuestions[counter]);
+  let myButtonsDiv = $("#answer-buttons");
+  myButtonsDiv.html("");
+
+  let allMyButtons = [];
+
+  allMyButtons.push(
+    $("<button>")
+      .html(correctAnswer[counter])
+      .click(gotItRight)
+  );
+  allMyButtons.push(
+    $("<button>")
+      .html(optionB[counter])
+      .click(gotItWrong)
+  );
+  allMyButtons.push(
+    $("<button>")
+      .html(optionC[counter])
+      .click(gotItWrong)
+  );
+  allMyButtons.push(
+    $("<button>")
+      .html(optionD[counter])
+      .click(gotItWrong)
+  );
+
+  shuffle(allMyButtons);
+
+  for (let i = 0; i < allMyButtons.length; i++) {
+    allMyButtons[i].appendTo(myButtonsDiv);
+  }
+
+  counter++;
+}
+
+$("#quizStart").on("click", function() {
+  loadPage();
+});
